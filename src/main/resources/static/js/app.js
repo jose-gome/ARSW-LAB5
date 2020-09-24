9f35d44b17034e9afec6853e1875e876a80a5029
Jose Luis Gomez Camacho <josemania10@gmail.com>
2020-09-23 21:00:46 -0500

    Seudoasientos

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,8 +2,7 @@ var app = (function () {
     var cinemaSeleccionado = "";
     var fecha = "";
     var listados = [];
-    var listadosDos = [];
-    var seats = []; 
+    var seats = [];
 
     setCinema = function(){
       cinemaSeleccionado = $("#name").val();
@@ -13,49 +12,57 @@ var app = (function () {
     }
     actulizarFunciones = function(){
       apimock.getFunctionsByCinemaAndDate(cinemaSeleccionado, fecha, mapObjetos);
+      $("#cineSelect").text("Cinema selected: "+cinemaSeleccionado);
     }
     
     var mapObjetos = function (listados){
-      console.log(listados);
       var name = listados.map(function(listado){
           $("#tabla > tbody").append(
-            "<tr>"+
-                    "<td id='movieName'>"+listado.movie.name+"</td>"+
-                    "<td>"+listado.movie.genre+"</td>"+
-                    "<td>"+listado.date+"</td>"+
-                    "<td><button type='button' onclick='app.drawSeats()' class='btn btn-primary'>Availability</button></td>"+
-            "</tr>"
+            `<tr>
+                    <td>${listado.movie.name}</td>
+                    <td>${listado.movie.genre}</td>
+                    <td>${listado.date}</td>
+                    <td><button type="button" onclick="app.getSeats(${listado.idFunction})" class="btn btn-primary">Open Seats</button></td>"
+            </tr>`
           );
           
       })
 
     }
-    getSeats = function(){
-      console.log($("#movieName").val());
-      apimock.getFunctionsByCinema($("#movieName").val(),drawSeats);
+    seats = function(idfuncion){
+      apimock.getFunctionsByCinema($("#name").val(),funciones, idfuncion);
     }
-    
-    var drawSeats = function(listadosDos){
-    
-      console.log(listadosDos);
-      //seats = listado.seats;
-      var name = listadosDos.map(function(listado){  
-        seats = listado.seats;  
-        var c = document.getElementById("myCanvas");
-        var lapiz = c.getContext("2d");
-        lapiz.fillStyle = "#0531ae";
-        lapiz.fillRect(20, 20 , 20, 20);
-        lapiz.beginPath();
-        for (var i = 0; i < seats.length; i++) {
-          for (var j = 0; j < seats[0].length; j++) {
-            lapiz.fillStyle = "#000000";  
-            if(seats[i][j]){
-              lapiz.fillStyle = "#999999"
+    var funciones = function(listados, idfuncion){
+      console.log(listados.functions.length);
+      for(var i = 0; i< listados.functions.length ; i++){
+        var funciones = listados.functions[i];
+        for (var key in funciones) {
+          if (key === "seats"){
+            seats = funciones[key];
+          }
+          if(key === "idFunction"){
+            if(funciones[key] === idfuncion){
+              drawSeats()
             }
-            lapiz.fillRect(i*40+45, j*20+105, 20, 20);
           }
         }
-      });
+      }
+    }
+    drawSeats = function(){
+      var c = document.getElementById("myCanvas");
+      var lapiz = c.getContext("2d");
+      lapiz.fillStyle = "#0531ae";
+      lapiz.fillRect(20, 20 , 820, 20);
+      lapiz.beginPath();
+      for (var i = 0; i < seats[0].length; i++) {
+        for (var j = 0; j < seats.length; j++) {
+          lapiz.fillStyle = "#000000";  
+          if(seats[j][i]){
+            lapiz.fillStyle = "#999999"
+          }
+          lapiz.fillRect(i*70+25, j*70+120, 40, 40);
+        }
+      }
     }
     
     var crearTabla = function(nombre, genero, tiempo, fecha){
@@ -65,6 +72,9 @@ var app = (function () {
     setCinema: setCinema,
     setFecha: setFecha,
     actulizarFunciones: actulizarFunciones,
-    getSeats : getSeats
+    getSeats :function(idFuncion){
+      seats (idFuncion);
+    }
+     
   }
 })();
